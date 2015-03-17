5275ae9b8d94abae83528b839061b72e097c115e
Louis Wilbrink <louis.wilbrink@gmail.com>
2015-03-17 17:08:32 -0400

    got promises to finish before rendering; admin page layout

--- api/services/utils.js
+++ api/services/utils.js
@@ -4,20 +4,30 @@
  */
 
 var Q = require('q');
+var _ = require('underscore');
 var Firebase = require('firebase');
 
 module.exports = {
 
     getRsvpCount: function () {
 
-        var rsvps = Q.defer();
+        var count = Q.defer();
 
         var rsvpsRef = new Firebase('https://highrock-easter-2015.firebaseio.com/rsvps');
         
         rsvpsRef.once('value', function (snapshot) {
-            rsvps.resolve('louis');
+            var rsvps = snapshot.val();
+            
+            // [first, second, third] service.
+            var guests = [0, 0, 0]
+
+            _.each(rsvps, function (rsvp) {
+                guests[rsvp.service - 1] += rsvp.guests;
+            });
+            
+            count.resolve(guests);
         });
 
-        return rsvps.promise;
+        return count.promise;
     }
 };
